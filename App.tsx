964a0bb9184ee8b300bf0d11a099188ee7a397b7
Gaya Kessler <gaya.kessler@gmail.com>
2019-05-22 13:32:19 +0200

    add application loader

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
+import { AppLoading } from 'expo';
 
 import AppNavigator from './src/AppNavigator';
 
 const AppContainer = createAppContainer(AppNavigator);
 
-class App extends React.Component {
+interface AppState {
+  isReady: boolean;
+}
+
+class App extends React.Component<{}, AppState> {
+  public state = {
+    isReady: false,
+  };
+
+  private async setup() {
+    return;
+  }
+
   public render() {
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this.setup}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={e => console.warn(e)}
+        />
+      );
+    }
+
     return <AppContainer />;
   }
 }
