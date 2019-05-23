011fccbc8eee4bc5502f2ba6aa4075ec59af553d
Gaya Kessler <gaya.kessler@gmail.com>
2019-05-23 08:49:17 +0200

    animate buttons on welcome

--- src/screens/Welcome/Welcome.tsx
+++ src/screens/Welcome/Welcome.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, Dimensions, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  ImageBackground,
+  Dimensions,
+  SafeAreaView,
+  Animated,
+  Easing,
+} from 'react-native';
 import { View, Text, H3, Button, Icon } from 'native-base';
 
 import Logo from '../../components/Logo/Logo';
@@ -34,15 +41,47 @@ const styles = StyleSheet.create({
   },
 });
 
-class Welcome extends React.Component {
+interface WelcomeState {
+  topAnim: Animated.Value;
+  bottomAnim: Animated.Value;
+}
+
+class Welcome extends React.Component<{}, WelcomeState> {
   private static navigationOptions = {
     header: null,
     title: 'Welcome',
   };
 
-  // check current login status
+  public state = {
+    bottomAnim: new Animated.Value(0),
+    topAnim: new Animated.Value(0),
+  };
+
+  public componentDidMount() {
+    Animated.timing(
+      this.state.topAnim,
+      {
+        delay: 300,
+        duration: 1000,
+        easing: Easing.out(Easing.ease),
+        toValue: 1,
+      },
+    ).start();
+
+    Animated.timing(
+      this.state.bottomAnim,
+      {
+        delay: 800,
+        duration: 1200,
+        easing: Easing.out(Easing.ease),
+        toValue: 1,
+      },
+    ).start();
+  }
 
   public render() {
+    const { topAnim, bottomAnim } = this.state;
+
     return (
       <ImageBackground
         source={require('./Scotland.jpg')}
@@ -51,32 +90,52 @@ class Welcome extends React.Component {
           width: '100%',
         }}
       >
-        <SafeAreaView style={{ flex: 1 }}>
-          <View style={styles.container}>
-            <View style={styles.top}>
-              <Logo width={Dimensions.get('window').width * 0.8} />
-              <View style={styles.introContainer}>
-                <Text>——</Text>
-                <H3 style={styles.introText}>
-                  Keep track of and rate the whiskies you drink
-                </H3>
-                <Text>——</Text>
-              </View>
+        <SafeAreaView style={styles.container}>
+          <Animated.View
+            style={{
+              ...styles.top,
+              opacity: topAnim,
+              transform: [{
+                translateY: topAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [-40, 0],
+                }),
+              }],
+            }}
+          >
+            <Logo width={Dimensions.get('window').width * 0.8} />
+            <View style={styles.introContainer}>
+              <Text>——</Text>
+              <H3 style={styles.introText}>
+                Keep track of and rate the whiskies you drink
+              </H3>
+              <Text>——</Text>
             </View>
-            <View style={styles.bottom}>
-              <Button iconLeft primary full style={{ marginBottom: 16 }}>
-                <Icon name="mail" />
-                <Text>Continue with Email</Text>
-              </Button>
-              <Button iconLeft info full style={{ marginBottom: 16 }}>
-                <Icon type="FontAwesome5" name="facebook-f" />
-                <Text>Login with Facebook</Text>
-              </Button>
-              <Button light transparent full>
-                <Text>New here? Create an account</Text>
-              </Button>
-            </View>
-          </View>
+          </Animated.View>
+          <Animated.View
+            style={{
+              ...styles.bottom,
+              opacity: bottomAnim,
+              transform: [{
+                translateY: bottomAnim.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [40, 0],
+                }),
+              }],
+            }}
+          >
+            <Button iconLeft primary full style={{ marginBottom: 16 }}>
+              <Icon name="mail" />
+              <Text>Continue with Email</Text>
+            </Button>
+            <Button iconLeft info full style={{ marginBottom: 16 }}>
+              <Icon type="FontAwesome5" name="facebook-f" />
+              <Text>Login with Facebook</Text>
+            </Button>
+            <Button light transparent full>
+              <Text>New here? Create an account</Text>
+            </Button>
+          </Animated.View>
         </SafeAreaView>
       </ImageBackground>
     );
