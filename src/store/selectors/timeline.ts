b817892fd4773e888421b3277ee69a9e3d4b9328
Gaya Kessler <gaya.kessler@gmail.com>
2019-06-10 16:11:33 +0200

    sort timeline items

--- src/store/selectors/timeline.ts
+++ src/store/selectors/timeline.ts
@@ -1,4 +1,10 @@
 export const getTimeline = (state: StoreShape): StoreTimeline => state.timeline;
 export const isTimelineLoading = (state: StoreShape): boolean => getTimeline(state).loading;
 export const isTimelineRefreshing = (state: StoreShape): boolean => getTimeline(state).refreshing;
-export const getTimelineItems = (state: StoreShape): StoreTimeline['items'] => state.timeline.items;
+export const getTimelineItems = (state: StoreShape): StoreTimeline['items'] => getTimeline(state)
+  .items
+  .sort((a, b) => {
+    if (a > b) return -1;
+    if (a < b) return 1;
+    return 0;
+  });
