4ac692f9725ef17fd0d92d0c9a32866ab9a69dcc
Gaya Kessler <gaya.kessler@gmail.com>
2019-06-11 11:56:19 +0200

    slainte dram on api

--- src/store/listeners/index.ts
+++ src/store/listeners/index.ts
@@ -1,12 +1,14 @@
 import { ReduxListenerMiddleware } from 'redux-listeners';
 
 import auth from './auth';
+import dram from './dram';
 import timeline from './timeline';
 import loader from '../loader/listeners';
 
 const createListeners = (listenMiddleware: ReduxListenerMiddleware) => {
   [
     ...auth,
+    ...dram,
     ...loader,
     ...timeline,
   ].forEach((listener) => {
