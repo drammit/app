bfbb38ae861d6e49c0c5b5ce7927170fda77029b
Gaya Kessler <gaya.kessler@gmail.com>
2019-06-17 09:17:00 +0200

    timeline to hooks

--- src/screens/Timeline/Timeline.tsx
+++ src/screens/Timeline/Timeline.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Button, Content,  Text } from 'native-base';
+import React, { useCallback } from 'react';
+import { Button, Content, Text } from 'native-base';
 import { NavigationInjectedProps } from 'react-navigation';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { dispatch } from '../../store/store';
 import { fetchTimeline, refreshTimeline } from '../../store/actions/timeline';
 import {
   getTimeline,
-  getTimelineItems, isTimelineEnd,
+  getTimelineItems,
+  isTimelineEnd,
   isTimelineLoading,
   isTimelineRefreshing,
 } from '../../store/selectors/timeline';
@@ -18,78 +18,68 @@ import Logo from '../../components/Logo/Logo';
 
 import colors from '../../config/colors';
 
-interface TimelineProps extends NavigationInjectedProps {
-  isEnd: boolean;
-  isLoading: boolean;
-  isRefreshing: boolean;
-  items: TimelineShape['items'];
-  timeline: TimelineShape;
-}
+type TimelineProps = NavigationInjectedProps;
 
-class TimelinePage extends React.Component<TimelineProps> {
-  private static navigationOptions = {
-    headerTitle: (
-      <Logo width={120} color={colors.light} />
-    ),
-    title: 'Drams',
-  };
+const TimelinePage = ({ navigation }: TimelineProps) => {
+  const dispatch = useDispatch();
+  const timeline = useSelector(getTimeline);
 
-  public onRefresh = (until: number) => {
-    dispatch(refreshTimeline({ until }));
+  const onRefresh = useCallback(
+    (until: number) => {
+      dispatch(refreshTimeline({ until }));
+    },
+    [],
+  );
+
+  const onFetch = useCallback(
+    (from?: number) => {
+      dispatch(fetchTimeline({ from }));
+    },
+    [],
+  );
+
+  const isLoading = isTimelineLoading(timeline);
+  const items = getTimelineItems(timeline);
+
+  if (!isLoading && items.length === 0) {
+    // redirect to WelcomeTour is focused
+    if (navigation.isFocused()) navigation.navigate('WelcomeTour');
   }
 
-  public onFetch = (from?: number) => {
-    dispatch(fetchTimeline({ from }));
-  }
-
-  public render() {
-    const { isLoading, items, navigation, timeline } = this.props;
-
-    if (!isLoading && items.length === 0) {
-      // redirect to WelcomeTour is focused
-      if (navigation.isFocused()) navigation.navigate('WelcomeTour');
-    }
-
-    return (
-      <SafeWithHeader style={{ flex: 1 }}>
-        <Timeline
-          timeline={timeline}
-          onFetch={this.onFetch}
-          onRefresh={this.onRefresh}
-          fallback={(
-            <Content key="intro" scrollEnabled={false} padder style={{ padding: 24 }}>
+  return (
+    <SafeWithHeader style={{ flex: 1 }}>
+      <Timeline
+        timeline={timeline}
+        onFetch={onFetch}
+        onRefresh={onRefresh}
+        fallback={(
+          <Content key="intro" scrollEnabled={false} padder style={{ padding: 24 }}>
+            <Text>
+              You have not added a dram review yet, or are not following anyone who has.{'\n'}
+              {'\n'}
+              Add your first review or start following people with reviews.
+            </Text>
+            <Button
+              style={{ marginTop: 24 }}
+              block
+              onPress={() => navigation.navigate('WelcomeTour')}
+            >
               <Text>
-                You have not added a dram review yet, or are not following anyone who has.{'\n'}
-                {'\n'}
-                Add your first review or start following people with reviews.
+                Read introduction
               </Text>
-              <Button
-                style={{ marginTop: 24 }}
-                block
-                onPress={() => navigation.navigate('WelcomeTour')}
-              >
-                <Text>
-                  Read introduction
-                </Text>
-              </Button>
-            </Content>
-          )}
-        />
-      </SafeWithHeader>
-    );
-  }
-}
-
-const mapStateToProps = (state: StoreShape) => {
-  const timeline = getTimeline(state);
-
-  return {
-    isEnd: isTimelineEnd(timeline),
-    isLoading: isTimelineLoading(timeline),
-    isRefreshing: isTimelineRefreshing(timeline),
-    items: getTimelineItems(timeline),
-    timeline,
-  };
+            </Button>
+          </Content>
+        )}
+      />
+    </SafeWithHeader>
+  );
 };
 
-export default connect(mapStateToProps)(TimelinePage);
+TimelinePage.navigationOptions = {
+  headerTitle: (
+    <Logo width={120} color={colors.light} />
+  ),
+  title: 'Drams',
+};
+
+export default TimelinePage;
